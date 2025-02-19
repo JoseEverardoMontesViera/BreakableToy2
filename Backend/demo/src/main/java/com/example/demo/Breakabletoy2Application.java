bcfa362b60fdcfe0b9e8af59b49ddfafa8c3282a
Everardo <everardo.montes@encora.com>
2025-02-19 11:47:59 -0600

    Springboot App generated

---
+++ Backend/demo/src/main/java/com/example/demo/Breakabletoy2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Breakabletoy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Breakabletoy2Application.class, args);
+		System.out.println("Estoy corriendo!!!");
+	}
+
+}
