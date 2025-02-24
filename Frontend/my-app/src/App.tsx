79e9b748da1b1edb37c50060838c441999412840
Everardo <everardo.montes@encora.com>
2025-02-24 13:51:58 -0600

    loginPage

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import LoginPage, { Username, Password, Submit, Title, Logo, Reset } from '@react-login-page/page1';
+import Login from '@react-login-page/page1';
+import SpotifyLogo from './imgs/spotifyLogo.png';
+
 import './App.css';
 
 function App() {
+  const styles = { height: 620 };
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='login'>
+        
+    <LoginPage>
+      <Username name="userUserName" visible={false}/>
+      <Password placeholder="请输入密码" name="userPassword" visible={false} />
+      <Submit>Login</Submit>
+      <Reset disabled visible={false}>Reset</Reset>
+      <Title keyname="title">eeeeee</Title>
+      <Title/>
+      <Login.Logo><img src="./imgs/spotifyLogo.png" alt="" /></Login.Logo>
+      <Login.Title>Login to Spotify</Login.Title>
+    </LoginPage>
+  </div>
     </div>
   );
 }
